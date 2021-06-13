37ce5c39fc78f744fec50c6585ce9cc0d523dd0e
Lau Chee Hoe <laulau.asia@gmail.com>
2021-06-13 12:07:09 +0800

    Add new file
    Code for states

---
+++ state.php
@@ -0,0 +1,12 @@
+//State - add to functions.php
+// Only one state e.g. PA in US
+
+add_filter( 'woocommerce_states', 'custom_woocommerce_states' );
+function custom_woocommerce_states( $states ) {
+$states['MY'] = array(
+'SGR' => __( 'Selangor', 'woocommerce' ),
+'KUL' => __( 'W.P. Kuala Lumpur', 'woocommerce' ),
+'PJY' => __( 'W.P. Putrajaya', 'woocommerce' )
+);
+return $states;
+}
