2186d0dd16bd8d7a08935a202547d4596dc6c98d
Lau Chee Hoe <laulau.asia@gmail.com>
2021-06-21 15:41:54 +0800

    Update woo-colors.php

--- woo-colors.php
+++ woo-colors.php
@@ -6,3 +6,15 @@ button.single_add_to_cart_button.button.alt {
     background-color: var( --e-global-color-primary );
     color: #fff;
 }
+
+.woocommerce #respond input#submit.alt:hover, .woocommerce a.button.alt:hover, .woocommerce button.button.alt:hover, .woocommerce input.button.alt:hover {
+    background-color: var( --e-global-color-5a5b874 );
+    color: var( --e-global-color-text );
+}
+
+
+main.checkoutwc .cfw-bottom-controls .cfw-primary-btn, main.checkoutwc .place-order .cfw-primary-btn, main.checkoutwc #info_payment button {
+    background-color: var( --e-global-color-primary );
+    color: #ffffff;
+}
+
