f618eecced11d5a57aadb0e2ba08439217425d30
Lau Chee Hoe <laulau.asia@gmail.com>
2021-06-21 15:36:02 +0800

    Create woo-colors.php

---
+++ woo-colors.php
@@ -0,0 +1,8 @@
+button.single_add_to_cart_button.button.alt {
+	background-color: var( --e-global-color-primary );
+}
+
+.woocommerce #respond input#submit.alt, .woocommerce a.button.alt, .woocommerce button.button.alt, .woocommerce input.button.alt {
+    background-color: var( --e-global-color-primary );
+    color: #fff;
+}
