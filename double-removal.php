e05fcd0b1438a5f5d4bd5a7196ea3e6f951abab8
Lau Chee Hoe <laulau.asia@gmail.com>
2021-09-11 22:35:46 +0800

    Update double-removal.php

--- double-removal.php
+++ double-removal.php
@@ -1,24 +1,9 @@
-// Remove the product description Title
-add_filter( 'woocommerce_product_description_heading', '__return_null' );
-
-
-// Change the product description title
-add_filter('woocommerce_product_description_heading', 'change_product_description_heading');
-function change_product_description_heading() {
- return __('NEW TITLE HERE', 'woocommerce');
+/* remove category */
+.single-product .product .product_meta span.posted_in {
+    display: none;
 }
 
-// Remove the additional information title
-add_filter( 'woocommerce_product_additional_information_heading', '__return_null' );
-
-// Change the additional information title
-function change_additional_information_heading() {
-    return __('NEW TITLE HERE', 'woocommerce');
+/* remove "Description" h2 from description tab */
+.woocommerce-tabs #tab-description h2 {
+    display: none;
 }
-add_filter( 'woocommerce_product_additional_information_heading', 'change_additional_information_heading' );
-
-
-.woocommerce-Reviews .woocommerce-Reviews-title{
-  display: none;
-}
-
