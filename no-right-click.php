21721ff2bcc8960c614e21f3a1f77d116153c93f
Lau Chee Hoe <laulau.asia@gmail.com>
2021-10-23 12:55:00 +0800

    Create no-right-click.php

---
+++ no-right-click.php
@@ -0,0 +1,12 @@
+	<script type="text/javascript">
+		$(document).ready(function () {
+			//Disable cut copy paste
+			$('body').bind('cut copy paste', function (e) {
+				e.preventDefault();
+			});
+			//Disable mouse right click
+			$("body").on("contextmenu",function(e){
+				return false;
+			});
+		});
+		</script>
