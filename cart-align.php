9ee27cf9bbd3cdff7fc30768bbff7e96dd85e603
Lau Chee Hoe <laulau.asia@gmail.com>
2023-08-13 20:25:02 +0800

    Update cart-align.php

--- cart-align.php
+++ cart-align.php
@@ -1,3 +1,12 @@
+/* Elementor Replaced */
+Product Title >> Advanced >> Layout >> Size >> ... >> Flex Grow 1 Flex Shrink 1
+
+
+
+
+
+
+
 /* Force 2 lines */
 .woocommerce ul.products li.product h2 {
   line-height: 2.5ex;
